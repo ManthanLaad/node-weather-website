9755f6c2f5b40a11f4a3e749ef3b484bcba57108
Manthan Laad <laad.manthan@gmail.com>
2021-06-03 21:13:46 +0530

    Heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,7 @@ form.addEventListener("submit", (event) => {
   const location = search.value;
   title.textContent = `On our way to ${location} 🚀`;
   message.textContent = ``;
-  fetch(
-    `http://localhost:3000/weather?address=${encodeURIComponent(location)}`
-  ).then((res) => {
+  fetch(`/weather?address=${encodeURIComponent(location)}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
         title.innerText = "Error";
